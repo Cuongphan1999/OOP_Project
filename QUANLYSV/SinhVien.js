8cb18792f1fb08e6bacee8d499cec53f7878e204
Cuongphan1999 <phanxuancuong00677@gmai.com>
2023-07-30 16:34:25 +0700

    done

---
+++ QUANLYSV/SinhVien.js
@@ -0,0 +1,8 @@
+function SinhVien(masv,hoten,email,sodt,cmnd)
+{
+    this.MaSV = masv; //thuoc tinh MaSV , gia tri tham so truyen vao masv
+    this.HoTen = hoten;
+    this.Email = email;
+    this.SoDT = sodt;
+    this.CMND = cmnd;
+}
